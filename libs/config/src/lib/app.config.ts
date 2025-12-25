1b78d345b85213f78007f448854b1fc25d6ce23f
heal <make.abundant@gmail.com>
2025-12-26 02:10:28 +0900

    init: libs/config

---
+++ libs/config/src/lib/app.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('app', () => ({
+  nodeEnv: process.env.NODE_ENV,
+  name: process.env.APP_NAME,
+  port: parseInt(process.env.APP_PORT || "3000", 10),
+  apiPrefix: process.env.API_PREFIX || 'api',
+
+  jwtSecret: process.env.JWT_SECRET || 'doyouwannabuildasnowman',
+  jwtAccessExpire: process.env.JWT_EXPIRE_TIME_ACCESS || '30m',
+  jwtRefreshExpire: process.env.JWT_EXPIRE_TIME_REFRESH || '15d',
+}));
