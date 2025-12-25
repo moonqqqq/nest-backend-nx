6e4a2758928ab01d1ff8a290cf6d2cb3a072becc
heal <make.abundant@gmail.com>
2025-12-25 22:31:31 +0900

    cicd: 커밋린트 추가

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "type-enum": [2, "always", ["hotfix", "cicd", "chore", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "add", "init", "test", "temp", "cicd"]],
+        "subject-case": [0]
+    }
+};
