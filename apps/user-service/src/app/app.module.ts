a11ad2af105a3a29ab340c13d4a756568f31c42b
heal <make.abundant@gmail.com>
2025-12-27 15:19:52 +0900

    chore: user-service 기본 구성 수정

--- apps/user-service/src/app/app.module.ts
+++ apps/user-service/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { DatabaseModule } from '@libs/database';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [],
-  providers: [UserService],
+  providers: [],
 })
-export class UserModule {}
+export class AppModule { }
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -5,10 +5,10 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from './app/user.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserModule);
+  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
