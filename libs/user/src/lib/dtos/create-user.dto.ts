a039d11508a92f62f46cf369cf783b6a586a4858
heal <make.abundant@gmail.com>
2026-01-03 23:21:37 +0900

    fix: validationPipe 작동확인

--- libs/user/src/lib/dtos/create-user.dto.ts
+++ libs/user/src/lib/dtos/create-user.dto.ts
@@ -1,7 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class CreateUserPayload {
-  constructor(
-    readonly email: string,
-    readonly password: string,
-    readonly name: string,
-  ) {}
+  @ApiProperty({ example: 'hi@gmail.com' })
+  @IsString()
+  readonly email: string;
+
+  @ApiProperty({ example: 'password123' })
+  @IsString()
+  readonly password: string;
+
+  @ApiProperty({ example: 'heal kim' })
+  @IsString()
+  readonly name: string;
+
+  constructor(email: string, password: string, name: string) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+  }
 }
