b1f64adcc87c3f991f7fadedda9ad5a9b025ef25
heal <make.abundant@gmail.com>
2026-01-06 12:13:38 +0900

    refactor: app module 정리

--- apps/user-service/src/app/app.module.ts
+++ apps/user-service/src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@libs/database';
 import { UserModule } from './modules/user/user.module';
-import { UserController } from './modules/user/user.controller';
-import { IUserService } from './modules/user/interfaces/user-service.interface';
-import { UserService } from './modules/user/user.service';
-import { IUserRepository } from './modules/user/interfaces/user-repository.interface';
-import { UserRepository } from './modules/user/user.repository';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfig, ServiceConfig } from '@libs/config';
 import { LoggerModule } from '@libs/logger';
@@ -20,16 +15,7 @@ import { LoggerModule } from '@libs/logger';
     UserModule,
     LoggerModule,
   ],
-  controllers: [UserController],
-  providers: [
-    {
-      provide: IUserService,
-      useClass: UserService,
-    },
-    {
-      provide: IUserRepository,
-      useClass: UserRepository,
-    },
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
