af9435de80ecc7ce34ed135af646cb6a17d5ad00
heal <make.abundant@gmail.com>
2026-01-07 12:41:28 +0900

    add: AppEventType에 stream 관련 이벤트 추가

--- libs/shared/src/lib/types/app-event.typs.ts
+++ libs/shared/src/lib/types/app-event.typs.ts
@@ -1,6 +1,12 @@
 export const AppEventType = {
-  LLM_MESSAGE_CREATED: 'LLM_MESSAGE_CREATED',
-  LLM_ANSWER_MESSAGE_RECEIVED: 'LLM_ANSWER_MESSAGE_RECEIVED',
+  LLM_MESSAGE_CREATED: 'llm_message_created',
+  LLM_ANSWER_MESSAGE_RECEIVED: 'llm_answer_message_received',
+
+  // stream event
+  STREAM_STARTED: 'stream_started',
+  STREAM_MESSAGE: 'stream_message',
+  STREAM_COMPLETED: 'stream_completed',
+  STREAM_FAILED: 'stream_failed',
 } as const;
 
 export type TAppEventType = (typeof AppEventType)[keyof typeof AppEventType];
