84923ab2a46322785db1fb1e63c51ec7e6194ec5
heal <make.abundant@gmail.com>
2026-01-14 16:16:16 +0900

    add: kafka topic - LLM_ANSWER_FAILED

--- libs/event-stream/src/lib/constants/topic.constant.ts
+++ libs/event-stream/src/lib/constants/topic.constant.ts
@@ -1,6 +1,7 @@
 export const Topic = {
   USER_LLM_MESSAGE_CREATED: 'user-llm-message.created',
   LLM_ANSWER_MESSAGE_COMPLETED: 'llm-answer-message.completed',
+  LLM_ANSWER_FAILED: 'llm-answer-message.failed', // DLQ 용
 } as const;
 
 export type TTopic = (typeof Topic)[keyof typeof Topic];
